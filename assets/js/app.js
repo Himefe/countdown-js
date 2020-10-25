0df430e6e0c46e88762d1c4e3179670a4e9eaade
Higor Almeida <idanger2015@hotmail.com>
2020-10-24 23:03:09 -0300

    Projeto Contagem Regressiva para o Natal

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+import Countdown from './modules/countdown.js';
+
+const diasProNatal = new Countdown('24 December 2020 23:59:59 GMT-0300');
+console.log(diasProNatal.total);
+
+const diasHtml = document.querySelector('.diasNumero');
+const horasHtml = document.querySelector('.horasNumero');
+const minutosHtml = document.querySelector('.minutosNumero');
+const segundosHtml = document.querySelector('.segundosNumero');
+
+setInterval(() => {
+    diasHtml.innerText = diasProNatal.total.days;
+    horasHtml.innerText = diasProNatal.total.hours;
+    const attMinutes =  minutosHtml.innerText = diasProNatal.total.minutes;
+    const attSeconds = segundosHtml.innerText = diasProNatal.total.seconds;
+    
+
+}, 1000);
+
+
+
+
+
+
+
---
+++ assets/js/modules/countdown.js
@@ -0,0 +1,40 @@
+export default class Countdown {
+    constructor(futureDate){
+        this.futureDate = futureDate;
+    }
+    get _actualDate(){
+        return new Date();
+    }
+    get _futuredate(){
+        return new Date(this.futureDate);
+    }
+    get _timeTempDiff(){
+        return this._futuredate.getTime() - this._actualDate.getTime();
+    }
+    get days(){
+        return Math.floor(this._timeTempDiff / (24 * 60 * 60 * 1000));
+    }
+    get hours(){
+        return Math.floor(this._timeTempDiff / (60 * 60 * 1000));
+    }
+    get minutes(){
+        return Math.floor(this._timeTempDiff / (60 * 1000));
+    }
+    get seconds(){
+        return Math.floor(this._timeTempDiff / 1000);
+    }
+    get total(){
+        const days = this.days;
+        const hours = this.hours % 24;
+        const minutes = this.minutes % 60;
+        const seconds = this.seconds % 60;
+        return {
+            days,
+            hours,
+            minutes,
+            seconds
+        }     
+    }
+}
+
+
